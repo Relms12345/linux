e16f120f7540969e9b9c255909ff1ed752007859
Ondrej Jirman <megi@xff.cz>
2022-10-29 23:12:49 +0200

    iio: st_sensors: Don't report error when the device is not present on I2C bus
    On Pinephone, this is not an error, because there are two alternate
    magentometer configurations.
    
    Signed-off-by: Ondrej Jirman <megi@xff.cz>

--- drivers/iio/common/st_sensors/st_sensors_core.c
+++ drivers/iio/common/st_sensors/st_sensors_core.c
@@ -611,8 +611,16 @@ int st_sensors_verify_id(struct iio_dev *indio_dev)
 		err = regmap_read(sdata->regmap,
 				  sdata->sensor_settings->wai_addr, &wai);
 		if (err < 0) {
+			if (err == -ENXIO) {
+				dev_info(&indio_dev->dev,
+					 "%s is not present on the bus\n",
+					 indio_dev->name);
+				return err;
+			}
+
 			return dev_err_probe(parent, err,
-					     "failed to read Who-Am-I register.\n");
+					     "%s: failed to read Who-Am-I register.\n",
+					     indio_dev->name);
 		}
 
 		if (sdata->sensor_settings->wai != wai) {
