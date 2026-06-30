375377f708429751386b545caf369eac546352b1
Alastair D'Silva <alastair@d-silva.org>
2026-06-30 21:16:30 +1000

    wifi: rtw88: sdio: Fix RCU stalls / interrupt storm by clearing RX_REQUEST interrupt
    In rtw_sdio_handle_interrupt(), the HISR status register is cleared using
    W1C (Write 1 to Clear) semantics. However, the driver masks out the
    REG_SDIO_HISR_RX_REQUEST bit in the local 'hisr' variable before writing
    it back, causing a 0 to be written to that bit. This prevents the RX request
    interrupt from being cleared, trapping the CPU core in an infinite interrupt
    storm loop (starving the RCU preempt kthread and locking up the system).
    
    Remove the masking of REG_SDIO_HISR_RX_REQUEST so that the interrupt status
    is correctly written back as a 1 and acknowledged by the hardware.
    
    Signed-off-by: Alastair D'Silva <alastair@d-silva.org>
    Assisted-by: Antigravity <antigravity@google.com>

--- drivers/net/wireless/realtek/rtw88/sdio.c
+++ drivers/net/wireless/realtek/rtw88/sdio.c
@@ -1085,7 +1085,6 @@ static void rtw_sdio_handle_interrupt(struct sdio_func *sdio_func)
 	if (hisr & REG_SDIO_HISR_TXERR)
 		rtw_sdio_tx_err_isr(rtwdev);
 	if (hisr & REG_SDIO_HISR_RX_REQUEST) {
-		hisr &= ~REG_SDIO_HISR_RX_REQUEST;
 		rtw_sdio_rx_isr(rtwdev);
 	}
 
